19dd3087b5a47f4e27de4a11ccc3a814634c7f4c
Lazar-Djukovic <ldjukovic40@gmail.com>
2021-10-30 20:03:09 +0100

    House
    House fully completed, with day and night cycle and brick graphics.
     Pong basis complete but not uploaded yet.

--- pygame/myfirstpygame.py
+++ pygame/myfirstpygame.py
@@ -3,24 +3,36 @@ import pygame
 
 # -- Colours
 BLACK = (0,0,0)
-WHITE = (255,255,255)
-BLUE = (50,50,255)
+BLUE = (100,100,255)
 YELLOW = (255,255,0)
-
+BROWN = (210,105,30)
+GREEN = (80,255,40)
+RED = (175,13,8)
+BRICK = (245,85,57)
+GRAY = (161, 165, 162)
+LBLUE = (135,206,235)
+DBLUE = (0,33,66)
 # -- Initialise PyGame
+
 pygame.init()
 size = (640,480)
 screen = pygame.display.set_mode(size)
 # -- Title of new window/screen
+
 pygame.display.set_caption("My Window")
 # -- Exit game flag set to false
+day = True
+rise = True
+cyp = 100
+yy = 465
 done = False
-sun_x = 40
-sun_y = 100
+counter = 0
 # -- Manages how fast screen refreshes
+
 clock = pygame.time.Clock() 
 cxp = 40
 ### -- Game Loop
+
 while not done:
  # -- User input and controls
  for event in pygame.event.get():
@@ -29,19 +41,67 @@ while not done:
 #End If
  #Next event
  # -- Game logic goes after this comment
- if cxp > 680:
-     cxp = 40
+
+ if day == True:
+    cxp = cxp + 1
+
+ if cyp < 70 and day == True:
+    rise = False
  else:
-        cxp = cxp
- cxp = cxp + 1
- # -- Screen background is BLACK
- screen.fill (BLACK)
+    cyp = cyp - 0.1
+    cxp = cxp - 0.1
+   
+ if rise == False:
+    cyp = cyp + 0.2
+    cxp = cxp + 0.1
+ else: 
+    rise = True
+
+ if day == False:
+    screen.fill (DBLUE)
+ else:
+    screen.fill (LBLUE)
+
+ if cxp > 680:
+     day = False
+     counter = counter + 1
+ else:
+  cxp = cxp
+
+ if counter == 100:
+    day = True
+    cxp = 40
+    cyp = 100
+    rise = True
+    counter = 0
+    
+
+
+ 
+
+
  # -- Draw here
- pygame.draw.rect(screen, BLUE, (220,165,200,150))
- pygame.draw.circle(screen, YELLOW, (cxp,100),40,0)
+
+ pygame.draw.rect(screen, BRICK, (220,165,200,150))
+ pygame.draw.circle(screen, YELLOW, (cxp,cyp),40,0)
+ for yy in range (165,320,13):
+    pygame.draw.line(screen, GRAY, (220,yy),(420,yy),2)
+ for xx in range (220,420,33):
+    pygame.draw.line(screen, GRAY, (xx,165),(xx,320),2)
+ pygame.draw.rect(screen, BROWN, (300,250,40,65))
+ pygame.draw.rect(screen, BLUE, (240,190,30,25))
+ pygame.draw.rect(screen, BLUE, (240,240,30,25))
+ pygame.draw.rect(screen, BLUE, (370,190,30,25))
+ pygame.draw.rect(screen, BLUE, (370,240,30,25))
+ pygame.draw.polygon(screen, RED, ((220,166),(320,130),(420,166)))
+ pygame.draw.rect(screen, GREEN, (0,315,640,170))
+ 
  # -- flip display to reveal new position of objects
  pygame.display.flip()
+
  # - The clock ticks over
  clock.tick(60)
+
 #End While - End of game loop
+
 pygame.quit()
