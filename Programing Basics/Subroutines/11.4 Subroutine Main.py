6e97677f4c33d50d4438787df32615791fb6d1ad
Lazar-Djukovic <ldjukovic40@gmail.com>
2022-04-27 10:38:34 +0100

    Create 11.4 Subroutine Main.py

---
+++ Programing Basics/Subroutines/11.4 Subroutine Main.py
@@ -0,0 +1,17 @@
+def displaymenu():
+  print(' ')
+  print('1. Add name')
+  print('2. Display list')
+  print('3. Quit')
+  print(' ')
+  Choices = [0,1,2]
+  Choice = int(input('Select 1-3: '))
+  while Choice < 1 or Choice > 3:
+    print('Invalid choice')
+    Choice = int(input('Please re-enter choice: '))
+    print(' ')
+  return(Choice)
+
+
+
+choice = displaymenu()
